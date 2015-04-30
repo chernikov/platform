7f18cd1e81f87e37bab252e3a807dd061c9bd10d
Andrey Chernikov <chernikov@gmail.com>
2015-04-30 16:35:46 +0300

    Report #1

---
+++ platformAthletic/Models/Info/AttendanceReport.cs
@@ -0,0 +1,77 @@
+﻿using platformAthletic.Model;
+using StackExchange.Profiling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace platformAthletic.Models.Info
+{
+    public class AttendanceReport
+    {
+        public class Record
+        {
+            public int Position { get; set; }
+
+            public User User { get; set; }
+
+            public int Week { get; set; }
+
+            public int Month { get; set; }
+
+            public int Year { get; set; }
+
+            public int AllTime { get; set; }
+
+            public int SelectedPeriod { get; set; }
+        }
+
+        protected IRepository Repository = DependencyResolver.Current.GetService<IRepository>();
+
+        public SearchAttendanceReport Search { get; set; }
+
+        public List<Record> List { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int CountPage
+        {
+            get
+            {
+                return (int)decimal.Remainder(TotalCount, SearchNationalLeaderboard.PageSize) == 0 ? TotalCount / SearchNationalLeaderboard.PageSize : TotalCount / SearchNationalLeaderboard.PageSize + 1;
+            }
+        }
+
+        public AttendanceReport(SearchAttendanceReport search, Team team)
+        {
+            var profiler = MiniProfiler.Current; // it's ok if this is null
+            using (profiler.Step("Calc Attendance Report"))
+            {
+                List = new List<Record>();
+
+                search.TeamID = team.ID;
+                Search = search;
+                Search.Init();
+                Process();
+            }
+        }
+
+        public void Process()
+        {
+            var users = Repository.TeamPlayersUsers.Where(p => p.PlayerOfTeamID == Search.TeamID);
+
+            foreach (var user in users)
+            {
+                List.Add(new Record()
+                {
+                    User = user,
+                    Week = user.WeekAttendanceCount,
+                    Month = user.MonthAttendanceCount,
+                    Year = user.YearAttendanceCount,
+                    AllTime = user.AllAttendanceCount,
+                });
+            }
+        }
+    }
+}
---
+++ platformAthletic/Models/Info/SearchAttendanceReport.cs
@@ -0,0 +1,166 @@
+﻿using platformAthletic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace platformAthletic.Models.Info
+{
+    public class SearchAttendanceReport
+    {
+
+        public enum SortEnum
+        {
+            WeekAsc,
+            WeekDesc,
+            MonthAsc,
+            MonthDesc,
+            YearAsc,
+            YearDesc,
+            AllTimeAsc,
+            AllTimeDesc,
+        }
+
+        protected IRepository Repository = DependencyResolver.Current.GetService<IRepository>();
+
+
+        public int TeamID { get; set; }
+
+        public int Page { get; set; }
+
+        public SortEnum Sort { get; set; }
+
+        public int? SportID { get; set; }
+
+        public int? FieldPositionID { get; set; }
+
+        public int? GroupID { get; set; }
+
+        public int? GradYear { get; set; }
+
+        public DateTime? StartPeriod { get; set; }
+
+        public DateTime? EndPeriod { get; set; }
+
+        public string SearchString { get; set; }
+
+        public int? StartID { get; set; }
+
+        public List<Sport> Sports { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListSportID
+        {
+            get
+            {
+                yield return new SelectListItem
+                {
+                    Value = "",
+                    Text = "All Sports",
+                    Selected = SportID == null
+                };
+
+                foreach (var sport in Sports)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = sport.ID.ToString(),
+                        Text = sport.Name,
+                        Selected = sport.ID == SportID
+                    };
+                }
+            }
+        }
+
+        public List<FieldPosition> FieldPositions { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListFieldPositionID
+        {
+            get
+            {
+                yield return new SelectListItem
+                {
+                    Value = "",
+                    Text = "All Positions",
+                    Selected = FieldPositionID == null
+                };
+                if (SportID != null)
+                {
+                    var positions = FieldPositions.Where(p => p.SportID == SportID.Value).ToList();
+                    foreach (var position in positions)
+                    {
+                        yield return new SelectListItem
+                        {
+                            Value = position.ID.ToString(),
+                            Text = position.Name,
+                            Selected = position.ID == FieldPositionID
+                        };
+                    }
+                }
+            }
+        }
+
+        public List<Group> Groups { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListGroupID
+        {
+            get
+            {
+                yield return new SelectListItem
+                {
+                    Value = "",
+                    Text = "All Groups",
+                    Selected = GroupID == null
+                };
+                if (Groups != null)
+                {
+                    foreach (var group in Groups)
+                    {
+                        yield return new SelectListItem
+                        {
+                            Value = group.ID.ToString(),
+                            Text = group.Name,
+                            Selected = group.ID == GroupID
+                        };
+                    }
+                }
+            }
+        }
+
+        public List<int> GradYears { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListGradYear
+        {
+            get
+            {
+                yield return new SelectListItem
+                {
+                    Value = "",
+                    Text = "All Years",
+                    Selected = GradYear == null
+                };
+                if (GradYears != null)
+                {
+                    foreach (var gradYears in GradYears)
+                    {
+                        yield return new SelectListItem
+                        {
+                            Value = gradYears.ToString(),
+                            Text = gradYears.ToString(),
+                            Selected = gradYears == GradYear
+                        };
+                    }
+                }
+            }
+        }
+
+        public void Init()
+        {
+            Sports = Repository.Sports.ToList();
+            FieldPositions = Repository.FieldPositions.ToList();
+            Groups = Repository.Groups.Where(p => p.TeamID == TeamID).ToList();
+            GradYears = Repository.TeamPlayersUsers.Where(p => p.PlayerOfTeamID == TeamID && p.GradYear.HasValue).Select(p => p.GradYear.Value).Distinct().ToList();
+
+        }
+    }
+}
