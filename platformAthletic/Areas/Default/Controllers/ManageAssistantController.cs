f3875fecb146252aff2aabbe1f01cdefae23d53a
chernikov <chernikov@gmail.com>
2015-07-12 16:47:09 +0300

    Fixes #10.Player On-boarding mobile

--- platformAthletic/Areas/Default/Controllers/ManageAssistantController.cs
+++ platformAthletic/Areas/Default/Controllers/ManageAssistantController.cs
@@ -62,7 +62,7 @@ namespace platformAthletic.Areas.Default.Controllers
                         (u, format) => format,
                         (u, format) => string.Format(format, u.Email, u.Password),
                         user);
-                    Repository.StartTutorial(user.ID);
+                    Repository.StartNormalMode(user.ID);
                 }
                 else
                 {
